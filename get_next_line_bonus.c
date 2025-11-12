ce89ce2fa45266eeb0359e132f9f454ec6a9fc3d
Mohamed Es-Seddik Gamal <mgamal@e2r6p8.1337.ma>
2025-11-12 14:20:02 +0100

    3rd

---
+++ get_next_line_bonus.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamal <mgamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 14:16:02 by mgamal            #+#    #+#             */
+/*   Updated: 2025/11/12 14:18:10 by mgamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*updated(char *arr)
+{
+	int		i;
+	char	*left;
+	int		total_lengh;
+	int		j;
+
+	i = 0;
+	if (!arr)
+		return (NULL);
+	while (arr[i] && arr[i] != '\n')
+		i++;
+	if (!arr[i])
+		return (NULL);
+	i++;
+	total_lengh = ft_strlen(arr);
+	left = malloc(total_lengh - i + 1);
+	if (!left)
+		return (NULL);
+	j = 0;
+	while (arr[i])
+		left[j++] = arr[i++];
+	left[j] = '\0';
+	return (left);
+}
+
+char	*read_write(int fd, char *buffer, char *left)
+{
+	char	*tmp;
+	int		b_r;
+
+	b_r = read(fd, buffer, BUFFER_SIZE);
+	while (b_r > 0)
+	{
+		buffer[b_r] = '\0';
+		tmp = ft_strjoin(left, buffer);
+		free(left);
+		left = tmp;
+		if (ft_strchr(left, '\n'))
+			break ;
+		b_r = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (b_r < 0)
+	{
+		free(left);
+		return (NULL);
+	}
+	return (left);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*left[1024];
+	char		*line;
+	char		*tmp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	left[fd] = read_write(fd, buffer, left[fd]);
+	free(buffer);
+	if (!left[fd])
+		return (NULL);
+	line = extracted_line(left[fd]);
+	tmp = updated(left[fd]);
+	free(left[fd]);
+	left[fd] = tmp;
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamal <mgamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 14:17:20 by mgamal            #+#    #+#             */
+/*   Updated: 2025/11/12 14:18:24 by mgamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*extracted_line(char *arr);
+
+#endif
